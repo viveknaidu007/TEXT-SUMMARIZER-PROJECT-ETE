672eae7881997881def4abc810178ccfd84c4e94
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-01-13 19:45:21 +0530

    data transformation added

---
+++ .history/src/textSummarizer/conponents/data_transformation_20240113193705.py
@@ -0,0 +1,10 @@
+
+
+import os
+from textSummarizer.logging import logger
+from transformers import AutoTokenizer
+from datasets import load_dataset, load_from_disk
+from textSummarizer.entity import DataTransformationConfig
+
+
+
